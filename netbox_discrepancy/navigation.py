daaafcb42c67d23dfeafacff8452f11924dc6ad2
Wouter de Bruijn <wouter@netco.nl>
2024-03-25 14:22:36 +0100

    💄 Changed order of navigation items

--- netbox_discrepancy/navigation.py
+++ netbox_discrepancy/navigation.py
@@ -2,13 +2,13 @@ from extras.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link="plugins:netbox_discrepancy:discrepancytype_list",
-        link_text="Discrepancy Types",
-        permissions=["netbox_discrepancy.view_discrepancytype"]
-    ),
-    PluginMenuItem(
         link="plugins:netbox_discrepancy:discrepancy_list",
         link_text="Discrepancies",
         permissions=["netbox_discrepancy.view_discrepancy"]
+    ),
+    PluginMenuItem(
+        link="plugins:netbox_discrepancy:discrepancytype_list",
+        link_text="Discrepancy Types",
+        permissions=["netbox_discrepancy.view_discrepancytype"]
     ),
 )
