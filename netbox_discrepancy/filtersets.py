469e794b5c645ceae3590d4852725fb103299c78
Wouter de Bruijn <wouter@netco.nl>
2024-03-25 14:23:40 +0100

    ✨ Added filters for Discrepancy model

---
+++ netbox_discrepancy/filtersets.py
@@ -0,0 +1,12 @@
+from netbox.filtersets import NetBoxModelFilterSet
+from .models import Discrepancy
+from django.db.models import Q
+
+class DiscrepancyFilterSet(NetBoxModelFilterSet):
+
+  class Meta:
+    model = Discrepancy
+    fields = ['device', 'type']
+
+  def search(self, queryset, name, value):
+    return queryset.filter(message__icontains=value)
--- netbox_discrepancy/forms.py
+++ netbox_discrepancy/forms.py
@@ -1,12 +1,20 @@
-from netbox.forms import NetBoxModelForm
+from django import forms
+from netbox.forms import NetBoxModelForm, NetBoxModelFilterSetForm
 from .models import Discrepancy, DiscrepancyType
+from dcim.models import Device
 
 class DiscrepancyTypeForm(NetBoxModelForm):
   class Meta:
     model = DiscrepancyType
-    fields = ['name', 'description', 'tags']
+    fields = ('name', 'description')
 
 class DiscrepancyForm(NetBoxModelForm):
   class Meta:
     model = Discrepancy
-    fields = ['device', 'type', 'message', 'tags']
+    fields = ('device', 'type', 'message')
+
+class DiscrepancyFilterForm(NetBoxModelFilterSetForm):
+  model = Discrepancy
+  
+  device = forms.ModelChoiceField(queryset=Device.objects.all(), required=False)
+  type = forms.ModelChoiceField(queryset=DiscrepancyType.objects.all(), required=False)
