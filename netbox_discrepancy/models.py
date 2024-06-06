5579337083ed19686d668608d77e1609b84612f5
Wouter de Bruijn <wouter@netco.nl>
2024-06-06 11:11:33 +0200

    🗃️ Increased namefield length to 100

--- netbox_discrepancy/models.py
+++ netbox_discrepancy/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 
 class DiscrepancyType(NetBoxModel):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
